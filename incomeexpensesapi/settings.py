f704db3218f792f8520f816ea1bed50a97238526
rameshTwilight <121216304+rameshTwilight@users.noreply.github.com>
2023-05-15 15:17:39 +0530

    Update settings.py
    set

--- incomeexpensesapi/settings.py
+++ incomeexpensesapi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)xxgb(ux-ui6zuskhis4!tq=80-*o_+2cozgr+)5up8%!=bp10"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -184,9 +184,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Email settings
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_FROM = "rameshkanna@twilightsoftwares.com"
-EMAIL_HOST_USER = "rameshkanna@twilightsoftwares.com"
-EMAIL_HOST_PASSWORD = "gxcwkhgqdnpzzfnc"
+EMAIL_FROM = ""
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 PASSWORD_RESET_TIMEOUT = 14400
